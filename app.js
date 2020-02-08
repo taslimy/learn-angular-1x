41f073f69510afe63a4e929dba3178fddd3b4293
taslimy <taslimer@gmail.com>
2020-02-08 17:05:41 -0500

    Dependency Injection

--- app.js
+++ app.js
@@ -2,8 +2,16 @@
 const myApp = angular.module("myApp", []); // function (takes a name (name of app), and an array of dependencies [])
 
 // first thing declare a controller # THIS IS THE MODEL #
-myApp.controller("mainController", () => {
+myApp.controller("mainController", () => {});
 
+let Person = function(firstname, lastname) {
+  this.firstname = firstname;
+  this.lastname = lastname;
+};
 
+let logPerson = person => {
+  console.log(person);
+};
 
-});
+let tas = new Person("Taslim", "Y");
+logPerson(tas);
