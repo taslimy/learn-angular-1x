94b0f62dd6f4fc21b0b9f2fbbeeccfe6033e90bf
taslimy <taslimer@gmail.com>
2020-02-08 16:42:54 -0500

    setting up app

---
+++ app.js
@@ -0,0 +1,9 @@
+// This initializes the app
+const myApp = angular.module("myApp", []); // function (takes a name (name of app), and an array of dependencies [])
+
+// first thing declare a controller # THIS IS THE MODEL #
+myApp.controller("mainController", () => {
+
+
+
+});
