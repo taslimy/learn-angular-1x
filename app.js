a89b238aa62bf9e1c4020e75bec985b45812d586
taslimy <taslimer@gmail.com>
2020-02-08 17:35:46 -0500

    depedency injection strings / function

--- app.js
+++ app.js
@@ -2,7 +2,13 @@
 const myApp = angular.module("myApp", []); // function (takes a name (name of app), and an array of dependencies [])
 
 // first thing declare a controller # THIS IS THE MODEL #
-myApp.controller("mainController", () => {
-
-  
+myApp.controller("mainController", $scope => {
+  console.log($scope);
 });
+
+// Injects scope and passes it the function via dependency injection
+const searchPeople = (firstName, $scope, height, age, occupation) => {
+  return "Taslim Y";
+};
+
+console.log(angular.injector().annotate(searchPeople));
